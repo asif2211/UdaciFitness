dab0e7f0d7267845f621333d7b7f747eb4768930
asif2211 <asifbsit@gmail.com>
2021-01-11 10:04:55 +0500

    udaciFitness project create method in helper getMetricMetaInfo

---
+++ components/UdaicStepper.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class UdaicStepper extends Component {
+    render() {
+        return (
+            <div>
+                UdaicStepper
+            </div>
+        )
+    }
+}
